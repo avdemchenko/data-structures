96ff88d5ff4176371565d6a01c017ba0a08e45a5
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-09-07 17:18:26 +0300

    Add demo for Adjacency Matrix

--- src/main/kotlin/graph/AdjacencyMatrix.kt
+++ src/main/kotlin/graph/AdjacencyMatrix.kt
@@ -6,19 +6,17 @@ import graph.core.Vertex
 
 class AdjacencyMatrix<T> : Graph<T> {
 
-    private val verticies = arrayListOf<Vertex<T>>()
+    private val vertices = arrayListOf<Vertex<T>>()
     private val weights = arrayListOf<ArrayList<Double?>>()
 
     override fun createVertex(data: T): Vertex<T> {
-        val vertex = Vertex(verticies.count(), data)
-        verticies.add(vertex)
-
+        val vertex = Vertex(vertices.count(), data)
+        vertices.add(vertex)
         weights.forEach {
             it.add(null)
         }
-
-        val row = ArrayList<Double?>(verticies.count())
-        repeat(verticies.count()) {
+        val row = ArrayList<Double?>(vertices.count())
+        repeat(vertices.count()) {
             row.add(null)
         }
         weights.add(row)
@@ -34,7 +32,7 @@ class AdjacencyMatrix<T> : Graph<T> {
         (0 until weights.size).forEach { column ->
             val weight = weights[source.index][column]
             if (weight != null) {
-                edges.add(Edge(source, verticies[column], weight))
+                edges.add(Edge(source, vertices[column], weight))
             }
         }
         return edges
@@ -42,4 +40,28 @@ class AdjacencyMatrix<T> : Graph<T> {
 
     override fun weight(source: Vertex<T>, destination: Vertex<T>): Double? =
         weights[source.index][destination.index]
+
+    fun visualise(): String {
+        val verticesDescription = vertices.joinToString(separator = "\n") { "${it.index}: ${it.data}" }
+        val verticesHeader = buildString {
+            append("\t\t")
+            (0 until weights.size).forEach { index -> append("$index\t\t\t") }
+        }
+
+        val grid = weights.mapIndexed { index, row ->
+            buildString {
+                append("$index\t\t")
+                (0 until weights.size).forEach { columnIndex ->
+                    val value = row[columnIndex]
+                    if (value != null) {
+                        append("$value\t\t")
+                    } else {
+                        append("0\t\t\t")
+                    }
+                }
+            }
+        }
+        val edgesDescription = grid.joinToString("\n")
+        return "$verticesDescription\n\n$verticesHeader\n\n$edgesDescription"
+    }
 }
--- src/main/kotlin/graph/GraphDemo.kt
+++ src/main/kotlin/graph/GraphDemo.kt
@@ -1,12 +1,42 @@
 import graph.AdjacencyList
+import graph.AdjacencyMatrix
 import graph.core.EdgeType.UNDIRECTED
 
 fun main(args: Array<String>) {
     showAdjacencyList()
+    showAdjacencyMatrix()
 }
 
 fun showAdjacencyList() {
     val graph = AdjacencyList<String>()
+
+    val kharkiv = graph.createVertex("Kharkiv")
+    val kyiv = graph.createVertex("Kyiv")
+    val lviv = graph.createVertex("Lviv")
+    val sanFrancisco = graph.createVertex("San Francisco")
+    val washingtonDC = graph.createVertex("Washington DC")
+    val dallas = graph.createVertex("Dallas")
+    val seattle = graph.createVertex("Seattle")
+
+    graph.add(UNDIRECTED, kharkiv, lviv, 500.0)
+    graph.add(UNDIRECTED, kharkiv, kyiv, 300.0)
+    graph.add(UNDIRECTED, lviv, kyiv, 100.0)
+    graph.add(UNDIRECTED, kyiv, washingtonDC, 1000.0)
+    graph.add(UNDIRECTED, lviv, sanFrancisco, 1500.0)
+    graph.add(UNDIRECTED, dallas, washingtonDC, 400.0)
+    graph.add(UNDIRECTED, sanFrancisco, washingtonDC, 350.0)
+    graph.add(UNDIRECTED, washingtonDC, seattle, 390.0)
+    graph.add(UNDIRECTED, sanFrancisco, seattle, 320.0)
+    graph.add(UNDIRECTED, dallas, sanFrancisco, 305.0)
+
+    println("\n========= Graph coordinates =======================\n")
+    println(graph.visualise())
+    println("\n========= Weight between Kharkiv and Kyiv =========\n")
+    println(graph.weight(kharkiv, kyiv))
+}
+
+fun showAdjacencyMatrix() {
+    val graph = AdjacencyMatrix<String>()
 
     val kharkiv = graph.createVertex("Kharkiv")
     val kyiv = graph.createVertex("Kyiv")
